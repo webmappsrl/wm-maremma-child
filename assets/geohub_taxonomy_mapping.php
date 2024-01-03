1bf49a10da511ff4e7111aa92b30d14a8930953b
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-01-03 09:55:50 +0100

    chore: taxonomies updated

--- assets/geohub_taxonomy_mapping.php
+++ assets/geohub_taxonomy_mapping.php
@@ -1,11 +1,13 @@
 <?php
 
-function geohub_taxonomy_mapping($taxonomy_name) {
+function geohub_taxonomy_mapping($taxonomy_name)
+{
     $mapping = $array = [
         'a piedi' => '1',
         'tours on foot' => '1',
         'in bicicletta' => '2',
-        'a cavallo' => '3'
+        'a cavallo' => '3',
+        'passeggiate' => '4'
     ];
     return $mapping[$taxonomy_name];
 }
