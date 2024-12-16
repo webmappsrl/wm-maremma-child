95d89d2510fe32cb8d629fea5d2cb2ac36157511
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2024-12-16 12:38:33 +0100

    chore: update ticket mapping and event feed handling oc: 4492
    Updated the ticket mapping JSON to remove purchase links for various tracks. Added functionality to exclude already sent events from the feed, schedule a cron job for marking unsent events as "sent," and removed immediate updates from the feed.

--- functions.php
+++ functions.php
@@ -123,3 +123,78 @@ function add_slick_slider()
   wp_enqueue_script('slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'add_slick_slider');
+
+/** 
+ * Escludi eventi già inviati dal feed
+ */
+function exclude_sent_events_from_feed($query)
+{
+  if ($query->is_feed() && $query->get('post_type') === 'tribe_events') {
+    $meta_query = array(
+      array(
+        'key'     => '_sent_in_feed',
+        'compare' => 'NOT EXISTS',
+      ),
+    );
+    $query->set('meta_query', $meta_query);
+  }
+}
+add_action('pre_get_posts', 'exclude_sent_events_from_feed');
+
+/** 
+ * Schedule a cron job to update the _sent_in_feed field
+ */
+add_action('init', 'schedule_mark_sent_events_cron');
+function schedule_mark_sent_events_cron()
+{
+  if (!wp_next_scheduled('mark_sent_events_cron')) {
+    $first_run = strtotime('11:14:00');
+    if ($first_run < time()) {
+      $first_run = strtotime('tomorrow 11:14:00'); // Make sure it is scheduled for tomorrow if the time has already passed
+    }
+    wp_schedule_event($first_run, 'daily', 'mark_sent_events_cron');
+  }
+}
+
+/** 
+ * Function to mark unsent events as "sent"
+ */
+add_action('mark_sent_events_cron', 'mark_events_as_sent_in_feed');
+function mark_events_as_sent_in_feed()
+{
+  $args = array(
+    'post_type'   => 'tribe_events',
+    'meta_query'  => array(
+      array(
+        'key'     => '_sent_in_feed',
+        'compare' => 'NOT EXISTS',
+      ),
+    ),
+    'posts_per_page' => -1,
+  );
+
+  $events = get_posts($args);
+
+  foreach ($events as $event) {
+    update_post_meta($event->ID, '_sent_in_feed', current_time('mysql'));
+  }
+}
+
+/** 
+ * Remove the immediate update from the feed
+ */
+remove_action('rss2_item', 'mark_event_as_sent_in_feed');
+
+// Delete the existing cron job
+// add_action('init', 'reset_mark_sent_events_cron');
+// function reset_mark_sent_events_cron() {
+//     if (wp_next_scheduled('mark_sent_events_cron')) {
+//         wp_clear_scheduled_hook('mark_sent_events_cron');
+//     }
+//     // Re-record the cron job for 
+//     $first_run = strtotime('19:00:00');
+//     if ($first_run < time()) {
+//         $first_run = strtotime('tomorrow 19:00:00');
+//     }
+//     wp_schedule_event($first_run, 'daily', 'mark_sent_events_cron');
+// }
